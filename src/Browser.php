b5ae394c81a0a54dfc5a09dc354835f3b6d43a37
Andrew Berry <deviantintegral@gmail.com>
2019-08-08 19:53:25 -0400

    Add a Browser class

---
+++ src/Browser.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deviantintegral\Har;
+
+/**
+ * Browser and creator share the same structure.
+ *
+ * @see http://www.softwareishard.com/blog/har-12-spec/#browser
+ */
+final class Browser extends Creator
+{
+}
