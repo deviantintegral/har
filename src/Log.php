269aa4a755a7761ab80eabab525906ddc923add7
Andrew Berry <deviantintegral@gmail.com>
2019-08-09 08:03:19 -0400

    Add missing Log class

---
+++ src/Log.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deviantintegral\Har;
+
+/**
+ * Represents the root HTTP Archive node.
+ *
+ * @see http://www.softwareishard.com/blog/har-12-spec/#log
+ */
+class Log
+{
+    /**
+     * Support the finest \DateTime precision we can.
+     */
+    public const ISO_8601_MICROSECONDS = 'Y-m-d\TH:i:s.uO';
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    private $creator;
+    private $browser;
+    private $pages;
+    private $entries;
+
+    /**
+     * @var string
+     */
+    private $comment;
+}
