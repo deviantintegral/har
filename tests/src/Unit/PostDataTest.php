3eee54a396d8e5dfbb8b96aa9dcc2e90f58d77c6
Andrew Berry <deviantintegral@gmail.com>
2020-11-20 17:02:19 -0500

    Don't initialize params to an empty array as it's optional

--- tests/src/Unit/PostDataTest.php
+++ tests/src/Unit/PostDataTest.php
@@ -15,17 +15,27 @@ class PostDataTest extends HarTestBase
     public function testSerialize()
     {
         $postData = (new PostData())
-          ->setParams([(new Params())->setName('test')->setFileName('test'), (new Params())->setName('test')->setContentType('text/plain')]);
+            ->setParams(
+                [
+                    (new Params())->setName('test')->setFileName('test'),
+                    (new Params())->setName('test')->setContentType(
+                        'text/plain'
+                    ),
+                ]
+            );
 
         $serializer = $this->getSerializer();
         $serialized = $serializer->serialize($postData, 'json');
         // We don't care about the interior of how 'params' are serialized, so
         // we just encode and decode them for the assert.
         $this->assertEquals(
-          [
-            'params' => json_decode($serializer->serialize($postData->getParams(), 'json'), true),
-          ],
-          json_decode($serialized, true)
+            [
+                'params' => json_decode(
+                    $serializer->serialize($postData->getParams(), 'json'),
+                    true
+                ),
+            ],
+            json_decode($serialized, true)
         );
 
         $class = PostData::class;
